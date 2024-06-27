34973b0957d444bb36d1f69b01c1b085cbab284d
Behindtheboard <jk2624@gmail.com>
2024-06-27 00:33:58 -0700

    Reutilize index.js as main control

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const homeButton = document.querySelector('#home-button');
 const menuButton = document.querySelector('#menu-button');
 const aboutButton = document.querySelector('#about-button');
 
+homeButton.addEventListener('click', () => {
+    document.querySelector('#content').appendChild(pageInit());
+});
 
-
-document.querySelector('#content').appendChild(main());
