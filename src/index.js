e475a3d15b77ed1d44db2dbcd6497e933ef79495
Behindtheboard <jk2624@gmail.com>
2024-06-26 23:38:18 -0700

    Basic HTML Layout with js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('nice');
+const content = document.querySelector('#content');
+const title = document.createElement('h1');
+title.textContent = 'Korean Southern Fried Chicken';
+content.appendChild(title);
+
+const p = document.createElement('p');
+p.textContent = 'This is the place where you want Korean Fried Chicken with your gravy and biscuits'
+content.appendChild(p);
+
+const backgroundImage = document.createElement('IMG');
+backgroundImage.setAttribute('id', 'background-image');
+backgroundImage.src = "./img/FriedChicken.jpg";
+backgroundImage.alt = "background fried chicken"
+content.appendChild(backgroundImage);
